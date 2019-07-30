f9897a2bbe4a01980a4dd466cb59836b6ac3736c
Milos Budalic <milos.budalic@persida-bio.com>
2019-07-30 16:59:32 +0200

    Added Navbar and made css changes.

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -5,9 +5,10 @@ import { Component } from "@angular/core";
     template:  `
     <div>
         <h1>Upcoming Angular Events</h1>
-        <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
-        <h3>{{thumbnail.someProperty}}</h3>
-        <button class="btn btn-primary"(click)="thumbnail.logFoo()">Log me some foo</button>
+        <div class="well">
+            <div>Hello World</div>
+        </div>
+        <event-thumbnail [event]="event1"></event-thumbnail>
     </div>
     `
 })
