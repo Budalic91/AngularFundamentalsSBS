1eb91e73240a310752a7ced61c0f6c24b726d82f
Milos Budalic <milos.budalic@persida-bio.com>
2019-08-01 15:01:57 +0200

    Pre-loading Data for Component: - Create resolver service - export class EventListResolver implements Resolve<any> -    Add constructor for service that we should use -  map from rxjs/operators -  Add resolve function (example): resolve() {         return this.eventService.getEvents().pipe(map(events => events));      } - In module import providers EventListResolver -  In route add following: { path: 'events', component: EventsListComponent, resolve: {events: EventListResolver} } -  Inside component add constructor ActivatedRoute and read events data: this.events = this.route.snapshot.data['events'];

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -12,19 +13,18 @@ import { ToastrService } from '../common/toastr.service';
                 <event-thumbnail (click)="handleThumbnailClick(event.name);" [event]="event"></event-thumbnail>
             </div>
         </div>
-        
     </div>
     `
 })
 export class EventsListComponent implements OnInit {
-    events: any[];
+    events: any;
 
-    constructor(private eventService: EventService, private toastr: ToastrService) {
+    constructor(private eventService: EventService, private toastr: ToastrService, private route:ActivatedRoute) {
         
     }
 
     ngOnInit() {
-        this.events = this.eventService.getEvents();
+        this.events = this.route.snapshot.data['events'];
     }
 
     handleThumbnailClick(eventName) {
