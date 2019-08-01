4e1f00fbdeb58349a829178487ce544ed9d65b2a
Milos Budalic <milos.budalic@persida-bio.com>
2019-08-01 13:52:28 +0200

    Link to route: - Base, - With param.

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 @Component({
     selector: 'event-thumbnail',
     template: `
-        <div class="well hoverwell thumbnail">
+        <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
             <h2> {{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
             <div [ngSwitch]="event?.time">
@@ -25,7 +25,7 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
         </div>
     `,
     styles: [`
-        .thumbnail {min-height: 210px; }
+        .thumbnail {min-height: 240px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb;}
     `]
